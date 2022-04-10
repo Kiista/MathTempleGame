172316d1ab0e32cb9a9f2f159eb42002298198de
Kiista <102416466+Kiista@users.noreply.github.com>
2022-04-10 15:01:12 +0200

    2nd Phase Code

---
+++ Assets/Scripts/QuizScripts/QuizGameManager.cs
@@ -0,0 +1,297 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class QuizGameManager : MonoBehaviour
+{
+    private Data data = new Data();
+
+    [SerializeField] GameEvents events = null;
+
+    [SerializeField] Animator timerAnimtor = null;
+    [SerializeField] TextMeshProUGUI timerText = null;
+    [SerializeField] Color timerHalfWayOutColor = Color.yellow;
+    [SerializeField] Color timerAlmostOutColor = Color.red;
+
+    private List<AnswerData> PickedAnswers = new List<AnswerData>();
+    private List<int> FinishedQuestions = new List<int>();
+    private int currentQuestion = 0;
+
+    private int timerStateParaHash = 0;
+
+    private IEnumerator IE_WaitTillNextRound = null;
+    private IEnumerator IE_StartTimer;
+    private Color timerDefaultColor = Color.white;
+    private GameManager gameManager;
+    private PlayerData currentPlayerData;
+
+
+    private bool IsFinished
+    {
+        get
+        {
+            return (FinishedQuestions.Count < data.Questions.Length) ? false : true;
+        }
+    }
+
+    private void OnEnable()
+    {
+        events.UpdateQuestionAnswer += UpdateAnswers;
+    }
+
+    private void OnDisable()
+    {
+        events.UpdateQuestionAnswer -= UpdateAnswers;
+    }
+
+    private void Awake()
+    {
+        events.CurrentFinalScore = 0;
+    }
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        currentPlayerData = gameManager.player1;
+
+        events.StartupHighScore = PlayerPrefs.GetInt(GameUtility.SavePrefKey);
+
+        timerDefaultColor = timerText.color;
+        LoadData();
+
+
+        timerStateParaHash = Animator.StringToHash("TimerState");
+
+        var seed = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
+        UnityEngine.Random.InitState(seed);
+
+        Display();
+    }
+
+    public void UpdateAnswers(AnswerData newAnswer)
+    {
+        if (data.Questions[currentQuestion].Type == AnswerType.Single)
+        {
+            foreach (var answer in PickedAnswers)
+            {
+                if (answer != newAnswer)
+                {
+                    answer.Reset();
+                }
+            }
+            PickedAnswers.Clear();
+            PickedAnswers.Add(newAnswer);
+        }
+        else
+        {
+            bool alreadyPicked = PickedAnswers.Exists(x => x == newAnswer);
+            if (alreadyPicked)
+            {
+                PickedAnswers.Remove(newAnswer);
+            }
+            else
+            {
+                PickedAnswers.Add(newAnswer);
+            }
+        }
+    }
+    public void EraseAnswers()
+    {
+        PickedAnswers = new List<AnswerData>();
+    }
+
+
+    void Display()
+    {
+        EraseAnswers();
+        var question = GetRandomQuestion();
+
+        if (events.UpdateQuestionUI != null)
+        {
+            events.UpdateQuestionUI(question);
+        }
+
+        if (question.UseTimer)
+        {
+            UpdateTimer(question.UseTimer);
+        }
+    }
+
+    public void Accept()
+    {
+        UpdateTimer(false);
+        bool isCorrect = CheckAnswers();
+        FinishedQuestions.Add(currentQuestion);
+
+        UpdateScore((isCorrect) ? data.Questions[currentQuestion].AddScore : -data.Questions[currentQuestion].AddScore);
+
+        if (currentPlayerData == gameManager.player1)
+        {
+            currentPlayerData = gameManager.player2;
+        }
+        else
+        {
+            currentPlayerData = gameManager.player1;
+        }
+
+        if (IsFinished)
+        {
+            SetHighScore();
+        }
+
+        var type = (IsFinished) ? UIManager.ResolutionScreenType.Finish : (isCorrect) ? UIManager.ResolutionScreenType.Correct : UIManager.ResolutionScreenType.Incorrect;
+
+        if (events.DisplayResolutionScreen != null)
+        {
+            events.DisplayResolutionScreen(type, data.Questions[currentQuestion].AddScore);
+        }
+
+        //AudioManager.instance.PlaySound((isCorrect) ? "CorrectSFX" : "IncorrectSFX");
+
+        if (type != UIManager.ResolutionScreenType.Finish)
+        {
+
+            if (IE_WaitTillNextRound != null)
+            {
+                StopCoroutine(IE_WaitTillNextRound);
+            }
+            IE_WaitTillNextRound = WaitTillNextRound();
+            StartCoroutine(IE_WaitTillNextRound);
+        }
+
+    }
+
+    private void UpdateTimer(bool state)
+    {
+        switch (state)
+        {
+            case true:
+                IE_StartTimer = StartTimer();
+                StartCoroutine(IE_StartTimer);
+
+                timerAnimtor.SetInteger(timerStateParaHash,2);
+                break;
+            case false:
+                if (IE_StartTimer != null) 
+                {
+                    StopCoroutine(IE_StartTimer);
+                }
+
+                timerAnimtor.SetInteger(timerStateParaHash, 1);
+                break;
+        }
+
+    }
+
+    IEnumerator StartTimer()
+    {
+        var totalTime = data.Questions[currentQuestion].Timer;
+        var timeLeft = totalTime;
+
+        timerText.color = timerDefaultColor;
+        while (timeLeft > 0)
+        {
+            timeLeft--;
+
+            //AudioManager.instance.PlaySound("CountdownSFX");
+
+            if (timeLeft < totalTime / 2 && timeLeft > totalTime /4)
+            {
+                timerText.color = timerHalfWayOutColor;
+            }
+            if (timeLeft < totalTime / 4)
+            {
+                timerText.color = timerAlmostOutColor;
+            }
+
+            timerText.text = timeLeft.ToString();
+            yield return new WaitForSeconds(1.0f);
+        }
+        Accept();
+    }
+
+    IEnumerator WaitTillNextRound()
+    {
+        yield return new WaitForSeconds(GameUtility.ResolutionDelayTime);
+        Display ();
+    }
+  
+
+    bool CheckAnswers()
+    {
+        if (!CompareAnswers())
+        {
+            return false;
+        }
+        return true;
+    }
+    bool CompareAnswers()
+    {
+        if (PickedAnswers.Count > 0)
+        {
+            List<int> c = data.Questions[currentQuestion].GetCorrectAnswers();
+            List<int> p = PickedAnswers.Select(x => x.AnswerIndex).ToList();
+
+            var f = c.Except(p).ToList();
+            var s = p.Except(c).ToList();
+
+            return !f.Any() && !s.Any();
+        }
+        return false;
+    }
+
+    void LoadData ()
+    {
+        data = Data.Fetch();
+    }
+
+    public void RestartAGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void SetHighScore()
+    {
+        var highscore = PlayerPrefs.GetInt(GameUtility.SavePrefKey);
+        if (highscore < events.CurrentFinalScore)
+        {
+            PlayerPrefs.SetInt(GameUtility.SavePrefKey, events.CurrentFinalScore);
+        }
+    }
+         
+    private void UpdateScore (int add)
+    {
+        currentPlayerData.numberOfCorrectAnswers += add;
+        if (currentPlayerData.numberOfCorrectAnswers < 0)
+        {
+            currentPlayerData.numberOfCorrectAnswers = 0;
+        }
+        events.ScoreUpdated?.Invoke(currentPlayerData.numberOfCorrectAnswers);
+    }
+
+    Question GetRandomQuestion()
+    {
+        var randomIndex = GetRandomQuestionIndex();
+        currentQuestion = randomIndex;
+
+        return data.Questions[currentQuestion];
+    }
+    int GetRandomQuestionIndex()
+    {
+        var random = 0;
+        if (FinishedQuestions.Count < data.Questions.Length)
+        {
+            do
+            {
+                random = UnityEngine.Random.Range(0, data.Questions.Length);
+            } while (FinishedQuestions.Contains(random) || random == currentQuestion);
+        }
+        return random;
+    }
+}
---
+++ Assets/Scripts/QuizScripts/UIManager.cs
@@ -0,0 +1,223 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+[Serializable()]
+public struct UIManagerParametres
+{
+    [Header("Answers Options")]
+    [SerializeField] float margins;
+    public float Margins { get { return margins; } }
+
+    [Header("Resolution Screen Options")]
+    [SerializeField] Color correctBGColor;
+    public Color CorrectBGColor { get { return correctBGColor; } }
+    [SerializeField] Color incorrectBGColor;
+    public Color IncorrectBGColor { get { return incorrectBGColor; } }
+    [SerializeField] Color finalBGColor;
+    public Color FinalBGColor { get { return finalBGColor; } }
+}
+[Serializable()]
+public struct UIElements
+{
+    [Header("answersContentArea")]
+    [SerializeField] RectTransform answersContentArea;
+    public RectTransform AnswersContentArea { get { return answersContentArea; } }
+
+    [Header("questionInfoTextObject")]
+    [SerializeField] TextMeshProUGUI questionInfoTextObject;
+    public TextMeshProUGUI QuestionInfoTextObject { get { return questionInfoTextObject; } }
+
+    [Header("scoreText")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    public TextMeshProUGUI ScoreText { get { return scoreText; } }
+    [Space]
+
+    [SerializeField] Animator resolutionScreenAnimator;
+    public Animator ResolutionScreenAnimator { get { return resolutionScreenAnimator; } }
+
+    [Header("resolutionBackground")]
+    [SerializeField] Image resolutionBackground;
+    public Image ResolutionBG { get { return resolutionBackground; } }
+
+    [Header("resolutionStateInfoText")]
+    [SerializeField] TextMeshProUGUI resolutionStateInfoText;
+    public TextMeshProUGUI ResolutionStateInfoText { get { return resolutionStateInfoText; } }
+
+    [Header("resolutionScoreText")]
+    [SerializeField] TextMeshProUGUI resolutionScoreText;
+    public TextMeshProUGUI ResolutionScoreText { get { return resolutionScoreText; } }
+
+    [Space]
+    [Header("highScoreText")]
+    [SerializeField] TextMeshProUGUI highScoreText;
+    public TextMeshProUGUI HighScoreText { get { return highScoreText; } }
+
+    [Header("mainCanvasGroup")]
+    [SerializeField] CanvasGroup mainCanvasGroup;
+    public CanvasGroup MainCanvasGroup { get { return mainCanvasGroup; } }
+
+    [Header("finishUIElements")]
+    [SerializeField] RectTransform finishUIElements;
+    public RectTransform FinishUIElements { get { return finishUIElements; } }
+
+}
+public class UIManager : MonoBehaviour
+{
+    public enum ResolutionScreenType { Correct, Incorrect, Finish }
+
+
+    [Header("References")]
+    [SerializeField] GameEvents events;
+
+    [Header("Answer Prefabs")]
+    [SerializeField] AnswerData answerPrefab;
+
+
+    [Space]
+    [SerializeField] UIElements uIElements;
+
+    [Space]
+    [SerializeField] UIManagerParametres parameters;
+
+
+    List<AnswerData> currentAnswers = new List<AnswerData>();
+    private int resStateParaHash = 0;
+
+    private IEnumerator IE_DisplayTimedResolution;
+
+    void OnEnable()
+    {
+        events.UpdateQuestionUI += UpdateQuestionUI;
+        events.DisplayResolutionScreen += DisplayResolution;
+        events.ScoreUpdated += UpdateScoreUI;
+
+    }
+    void OnDisable()
+    {
+        events.UpdateQuestionUI -= UpdateQuestionUI;
+        events.DisplayResolutionScreen -= DisplayResolution;
+        events.ScoreUpdated -= UpdateScoreUI;
+    }
+
+    private void Start()
+    {
+        UpdateScoreUI(0);
+        resStateParaHash = Animator.StringToHash("ScreenState");
+    }
+    void UpdateQuestionUI(Question question)
+    {
+        uIElements.QuestionInfoTextObject.text = question.info;
+        CreateAnswers(question);
+
+    }
+
+    void DisplayResolution(ResolutionScreenType type, int score)
+    {
+        UpdateResolutionUI(type, score);
+        uIElements.ResolutionScreenAnimator.SetInteger(resStateParaHash, 2);
+        uIElements.MainCanvasGroup.blocksRaycasts = false;
+
+        if (type != ResolutionScreenType.Finish)
+        {
+            if (IE_DisplayTimedResolution != null)
+            {
+                StopCoroutine(IE_DisplayTimedResolution);
+            }
+            IE_DisplayTimedResolution = DisplayTimeResolution();
+            StartCoroutine(IE_DisplayTimedResolution);
+        }
+    }
+
+    IEnumerator DisplayTimeResolution()
+    {
+        yield return new WaitForSeconds(GameUtility.ResolutionDelayTime);
+        uIElements.ResolutionScreenAnimator.SetInteger(resStateParaHash, 1);
+        uIElements.MainCanvasGroup.blocksRaycasts = true;
+    }
+
+    void UpdateResolutionUI(ResolutionScreenType type, int score)
+    {
+        var highscore = PlayerPrefs.GetInt(GameUtility.SavePrefKey);
+
+        switch (type)
+        {
+            case ResolutionScreenType.Correct:
+                uIElements.ResolutionBG.color = parameters.CorrectBGColor;
+                uIElements.ResolutionStateInfoText.text = "CORRECT";
+                uIElements.ResolutionScoreText.text = "+" + score;
+                break;
+            case ResolutionScreenType.Incorrect:
+                uIElements.ResolutionBG.color = parameters.IncorrectBGColor;
+                uIElements.ResolutionStateInfoText.text = "WRONG";
+                uIElements.ResolutionScoreText.text = "-" + score;
+                break;
+            case ResolutionScreenType.Finish:
+                uIElements.ResolutionBG.color = parameters.FinalBGColor;
+                uIElements.ResolutionStateInfoText.text = "FINAL SCORE";
+
+                StartCoroutine(CalculateScore());
+                uIElements.FinishUIElements.gameObject.SetActive(true);
+                uIElements.HighScoreText.gameObject.SetActive(true);
+                uIElements.HighScoreText.text = ((highscore > events.StartupHighScore) ? "new " : String.Empty) + "Highscore:" + highscore;
+                break;
+        }
+    }
+
+    IEnumerator CalculateScore()
+    {
+
+        if (events.CurrentFinalScore == 0)
+        {
+            uIElements.ResolutionScoreText.text = 0.ToString() ;
+            yield break;
+        }
+        var scoreValue = 0;
+
+        var scoreMoreThanZero = events.CurrentFinalScore > 0;
+
+        while ( scoreMoreThanZero ? scoreValue < events.CurrentFinalScore : scoreValue > events.CurrentFinalScore)
+        {
+            scoreValue += scoreMoreThanZero ? 1 : -1;
+            uIElements.ResolutionScoreText.text = scoreValue.ToString();
+
+            yield return null;
+        }
+    }
+
+    void CreateAnswers(Question question)
+    {
+        EraseAnswers();
+
+        float offset = 0 - parameters.Margins;
+        for (int i = 0; i < question.Answers.Length; i++)
+        {
+            AnswerData newAnswer = (AnswerData)Instantiate(answerPrefab, uIElements.AnswersContentArea);
+            newAnswer.UpdateData(question.Answers[i].Info, i );
+
+            newAnswer.Rect.anchoredPosition = new Vector2(0, offset);
+
+            offset -= (newAnswer.Rect.sizeDelta.y + parameters.Margins);
+            uIElements.AnswersContentArea.sizeDelta = new Vector2(uIElements.AnswersContentArea.sizeDelta.x, offset * -1);
+
+            currentAnswers.Add(newAnswer);
+        }
+    }
+
+    void EraseAnswers()
+    {
+        foreach (var answer in currentAnswers)
+        {
+            Destroy(answer.gameObject);
+        }
+        currentAnswers.Clear();
+    }
+
+    void UpdateScoreUI(int score)
+    {
+        uIElements.ScoreText.text = "Score: " + score;
+    }
+}
