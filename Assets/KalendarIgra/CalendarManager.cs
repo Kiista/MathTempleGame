88074dd86179153ccb0daf40e93ffe3bb754296e
Kiista <102416466+Kiista@users.noreply.github.com>
2022-05-05 00:26:59 +0200

    Update

--- Assets/KalendarIgra/CalendarManager.cs
+++ Assets/KalendarIgra/CalendarManager.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CalendarManager : MonoBehaviour
 {
     [SerializeField] private Dial outerDial;
     [SerializeField] private Dial innerDial;
+    [SerializeField] private GameObject showCodeButton;
 
-    private void Update () {
+    private void Update () 
+    {
         if (outerDial.GetChosenValue() == 5 && innerDial.GetChosenValue() == 10) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             Debug.Log("POBEDIO SI");
